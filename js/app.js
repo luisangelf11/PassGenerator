c4da56482d31680c1a4390e14cac3af917af4dab
luisangelf11 <71238653+luisangelf11@users.noreply.github.com>
2021-11-15 18:00:48 -0400

    Add Css and Code JS for global vars

---
+++ js/app.js
@@ -0,0 +1,5 @@
+//My global vars and consts
+const d = document,
+      $form = d.getElementById("generator"),
+      $password = d.getElementById("pass");
+let  $length = d.getElementById("size").value;
